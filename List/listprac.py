38917016c5ab96ab924c4ac62cf4a34474ceee40
MakaRonish <127733003+MakaRonish@users.noreply.github.com>
2024-07-07 00:07:40 -0400

    list

---
+++ List/listprac.py
@@ -0,0 +1,9 @@
+countries=["Nepal","Canada","Ghana","Australia"]
+# print(countries[-1])
+# print(countries[0:3])
+
+# countries[2]="India"
+# print(countries)
+# print(len(countries))
+
+
---
+++ List/method.py
@@ -0,0 +1,27 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = ["one", "two", "three"]
+# list1.extend(list2)
+# print(list1)
+
+# list2.append("four")
+# print(list2)
+
+# list1.insert(3,3.5)
+
+# list1.remove(3.5)
+# print(list1)
+
+# print(list1.index(1))
+
+list3 = [4, 6, 8, 3, 12, 4, 6, 8, 90]
+print(list3.sort())
+print(list3)
+list4 = list1.copy()
+
+print(list1)
+print(list4)
+list1.pop()
+print(list1)
+print(list4)
+list1.remove(1)
+print(len(list1))
