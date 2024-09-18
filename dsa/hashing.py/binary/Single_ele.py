ad7613ad69073d91706eb044503d61e93f013a47
MakaRonish <ronishmakaju12345@gmail.com>
2024-09-17 23:54:53 -0400

    Update Single_ele.py

--- dsa/hashing.py/binary/Single_ele.py
+++ dsa/hashing.py/binary/Single_ele.py
@@ -4,3 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        i = 0
+        while True:
+            if i == len(nums) - 1:
+                if nums[i] != nums[i - 1]:
+                    return nums[i]
+            if nums[i] != nums[i + 1]:
+                return nums[i]
+
+            else:
+                i += 2
+
+
+# optimal soln
+
+
+def singleNonDuplicate(nums):
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        middle = (low + high) // 2
