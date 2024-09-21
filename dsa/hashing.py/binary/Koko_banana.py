3270330b7a7405d607372e70a45dcdf416da88ff
MakaRonish <ronishmakaju12345@gmail.com>
2024-09-20 23:51:29 -0400

    Update Koko_banana.py

--- dsa/hashing.py/binary/Koko_banana.py
+++ dsa/hashing.py/binary/Koko_banana.py
@@ -1,7 +1,33 @@
+import math
+
+
 class Solution(object):
+    def total_hours(self, piles: list, rate):
+        total = 0
+        for i in range(len(piles)):
+            ans = math.ceil(piles[i] / rate)
+            total += ans
+        return total
+
     def minEatingSpeed(self, piles, h):
         """
         :type piles: List[int]
         :type h: int
         :rtype: int
+
         """
+        low = 1
+        high = max(piles)
+        while low <= high:
+            middle = (low + high) // 2
+            r = self.total_hours(piles, middle)
+            if r == h:
+                return middle
+            elif r < h:
+                high = middle - 1
+            else:
+                low = middle + 1
+
+
+a = Solution()
+print(a.minEatingSpeed([312884470], 312884470))
