e31f107c37c1641d6eef765f7c9cc8b4687f0d33
MakaRonish <ronishmakaju12345@gmail.com>
2024-09-25 21:07:13 -0400

    Update shipping.py

--- dsa/hashing.py/binary/shipping.py
+++ dsa/hashing.py/binary/shipping.py
@@ -1,17 +1,20 @@
 class Solution(object):
-    def num_of_days(self, weights, capacity):
-        Total_day = 0
-        ship = 0
-        weights_sum = 0
-        while ship < len(weights):
-            weights_sum += weights[ship]
-            if weights_sum > capacity:
-                weights_sum = weights[ship]
-                Total_day += 1
-            ship += 1
-        if weights_sum > 0:
-            Total_day += 1
-        return Total_day
+    def right_weight(self, weights, capacity):
+        i = 0
+        day_taken = 0
+        total = 0
+
+        while i < len(weights):
+            total += weights[i]
+            if total == capacity:
+                day_taken += 1
+                total = 0
+            elif total > capacity:
+                day_taken += 1
+                total = weights[i]
+            i += 1
+
+        return day_taken if total == 0 else day_taken + 1
 
     def shipWithinDays(self, weights, days):
         """
@@ -19,19 +22,18 @@ class Solution(object):
         :type days: int
         :rtype: int
         """
-        low = min(weights)
-        high = len(weights) * max(weights)
-        ans = 0
+        low = 0
+        high = sum(weights)
+        capacity = 0
         while low <= high:
             middle = (low + high) // 2
-            total_day = self.num_of_days(weights, middle)
-            if total_day <= days:
-                ans = middle
+            if self.right_weight(weights, middle) <= days:
+                capacity = middle
                 high = middle - 1
             else:
                 low = middle + 1
-        return ans
+        return capacity
 
 
 a = Solution()
-print(a.num_of_days([1, 2, 3, 1, 1], 3))
+print(a.shipWithinDays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
